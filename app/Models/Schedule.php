c0af4d10f853b34a995c3f14a19a3d0f81920436
rickyalfin <rickyalfina30@gmail.com>
2025-01-05 15:52:53 +0700

    first commit

---
+++ app/Models/Schedule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'shift_id',
+        'office_id',
+    ];
+
+    // BelongsTo digunakan untuk relasi antar tabel (foreign key)
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function shift(): BelongsTo
+    {
+        return $this->belongsTo(Shift::class);
+    }
+
+    public function office(): BelongsTo
+    {
+        return $this->belongsTo(Office::class);
+    }
+}
