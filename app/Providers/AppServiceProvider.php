e81b1fafa7204dca0d1a6aa59ac3112ff4e80e47
rickyalfin <rickyalfina30@gmail.com>
2025-05-24 21:28:25 +0700

    Menambahkan Library Laravel Pulse untuk Monitoring Sistem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (User $user) {
+            return $user->hasRole('Super Admin');
+        });
+
     }
 }
