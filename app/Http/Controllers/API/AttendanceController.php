27dfa94ec5cbaaba64be56062530f1a67ba43d08
rickyalfin <rickyalfina30@gmail.com>
2025-06-07 17:01:39 +0700

    Membuat API Store Attendance

--- app/Http/Controllers/API/AttendanceController.php
+++ app/Http/Controllers/API/AttendanceController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
 use App\Models\Schedule;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AttendanceController extends Controller
 {
@@ -51,5 +55,62 @@ class AttendanceController extends Controller
             'data'    => $schedule,
             'message' => 'Success get schedule',
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'latitude'  => 'required|numeric',
+            'longitude' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data'    => $validator->errors(),
+                'message' => 'Validation error',
+            ], 422);
+        }
+
+        $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+
+        if ($schedule) {
+            $attendance = Attendance::where('user_id', Auth::user()->id)
+                ->whereDate('created_at', date('Y-m-d'))->first();
+
+            if (! $attendance) {
+                $attendance = Attendance::create([
+                    'user_id'             => Auth::user()->id,
+                    'schedule_latitude'   => $schedule->office->latitude,
+                    'schedule_longitude'  => $schedule->office->longitude,
+                    'schedule_start_time' => $schedule->shift->start_time,
+                    'schedule_end_time'   => $schedule->shift->end_time,
+                    'start_latitude'      => $request->latitude,
+                    'start_longitude'     => $request->longitude,
+                    'start_time'          => Carbon::now()->toTimeString(),
+                    'end_time'            => Carbon::now()->toTimeString(),
+                ]);
+            } else {
+                $attendance->update([
+                    'end_latitude'  => $request->latitude,
+                    'end_longitude' => $request->longitude,
+                    'end_time'      => Carbon::now()->toTimeString(),
+                ]);
+            }
+
+            return response()->json([
+                'success' => true,
+                'data'    => $attendance,
+                'message' => 'Success store Attendance',
+            ]);
+
+        } else {
+            return response()->json([
+                'success' => false,
+                'data'    => null,
+                'message' => 'No schedule found',
+            ]);
+
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/get-attendance-today', [AttendanceController::class, 'getAttendanceToday']);
     Route::get('/get-schedule', [AttendanceController::class, 'getSchedule']);
+    Route::post('/store-attendance', [AttendanceController::class, 'store']);
 });
 
 Route::get('/user', function (Request $request) {
